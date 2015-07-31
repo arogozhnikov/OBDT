0433596ca04e12d36dc67ff315664803f6403feb
Alexey <iamfullofspam@gmail.com>
2015-07-31 17:49:51 +0300

    updated simple pruning

--- pruning/simple_pruner.py
+++ pruning/simple_pruner.py
@@ -1,16 +1,14 @@
 from __future__ import division, print_function, absolute_import
 
-
 __author__ = 'Alex Rogozhnikov'
 
 import numpy
 import copy
-from sklearn.metrics import roc_auc_score
 from six import BytesIO
 from . import utils
 
 try:
-    # To work both 
+    # To work we need both
     from pruning import _matrixnetapplier
 except:
     from rep.estimators import _matrixnetapplier
@@ -62,13 +60,14 @@ def compute_pessimistic_predictions(y_signed, predictions, tree_predictions, lea
     return predictions - y_signed * numpy.log((1. - sp) + sp * numpy.exp(-y_signed * learning_rate * tree_predictions))
 
 
-def select_trees(X, y, sample_weight, initial_mx_formula,
-                 loss_function,
-                 iterations=100,
-                 n_candidates=100, n_keptbest=0,
-                 learning_rate=0.1, selected_probability=1.,
-                 regularization=10.,
-                 verbose=False):
+def canonical_pruning(X, y, sample_weight, initial_mx_formula,
+                      loss_function,
+                      iterations=100,
+                      n_candidates=100,
+                      n_kept_best=0,
+                      learning_rate=0.1,
+                      regularization=10.,
+                      verbose=False):
     """
     Represents basic pruning algorithm, which greedily adds
     :param X: data
@@ -77,15 +76,14 @@ def select_trees(X, y, sample_weight, initial_mx_formula,
     :param initial_mx_formula:
     :param loss_function: loss function (following hep_ml convention for losses)
     :param iterations: int, how many estimators we shall leave
-    :param n_candidates: how many candidates we check on each iteration
-    :param n_keptbest: how many classifiers saved from previous iteration
+    :param n_candidates: how many candidate_trees we check on each iteration
+    :param n_kept_best: how many classifiers saved from previous iteration
     :param learning_rate: shrinkage, float
-    :param selected_probability: almost the same as shrinkage, but makes different steps for guessed and wrong steps.
     :param regularization: roughly, it is amount of event of each class added to each leaf. Represents a penalty
     :param verbose: bool, print stats at each step?
     :return: new OBDT list classifier.
     """
-    assert n_candidates > n_keptbest, "can't keep more then tested on each stage"
+    assert n_candidates > n_kept_best, "can't keep more then tested on each stage"
 
     # collecting information from formula
     old_trees = []
@@ -98,12 +96,6 @@ def select_trees(X, y, sample_weight, initial_mx_formula,
 
     # taking divisible by 8
     X, y, w = take_divisible(X, y, sample_weight=sample_weight)
-    y_signed = 2 * y - 1
-
-    # normalization of weight
-    w[y == 0] /= numpy.sum(w[y == 0])
-    w[y == 1] /= numpy.sum(w[y == 1])
-    w /= numpy.mean(w)
 
     # fitting loss function
     loss_function = copy.deepcopy(loss_function)
@@ -113,36 +105,36 @@ def select_trees(X, y, sample_weight, initial_mx_formula,
     pred = numpy.zeros(len(X), dtype=float)
     prev_iteration_best_ids = []  # ids of best trees at previous stages
     for iteration in range(iterations):
-        selected = numpy.random.choice(len(old_trees), replace=False, size=n_candidates - len(prev_iteration_best_ids))
-        selected = numpy.concatenate([selected, prev_iteration_best_ids]).astype(int)
-        candidates = [old_trees[i] for i in selected]
-        assert len(candidates) == n_candidates
+        new_candidate_trees = numpy.random.choice(len(old_trees), replace=False,
+                                                  size=n_candidates - len(prev_iteration_best_ids))
+        candidate_trees_indices = numpy.concatenate([new_candidate_trees, prev_iteration_best_ids]).astype(int)
+        candidate_trees = [old_trees[i] for i in candidate_trees_indices]
+        assert len(candidate_trees) == n_candidates
 
         grads = loss_function.negative_gradient(pred)
         hesss = loss_function.hessian(pred)
 
         candidate_losses = []
         candidate_new_trees = []
-        for tree in candidates:
+        for tree in candidate_trees:
             leaves = compute_leaves(X, tree)
             new_leaf_values = numpy.bincount(leaves, weights=grads, minlength=2 ** 6)
             new_leaf_values /= numpy.bincount(leaves, weights=hesss, minlength=2 ** 6) + regularization
-            new_tree = tree[0], tree[1], new_leaf_values
-            # for the sake of speed, here we use approximate step
-            effective_leaf_values = new_leaf_values * (learning_rate * selected_probability)
-            new_preds = pred + effective_leaf_values[leaves]
+            new_leaf_values *= learning_rate
+
+            new_preds = pred + new_leaf_values[leaves]
             candidate_losses.append(loss_function(new_preds))
+            new_tree = tree[0], tree[1], new_leaf_values
             candidate_new_trees.append(new_tree)
 
         # selecting one with minimal loss
         tree = candidate_new_trees[numpy.argmin(candidate_losses)]
         new_trees.append(tree)
-        prev_iteration_best_ids = numpy.take(selected, numpy.argsort(candidate_losses)[1:n_keptbest])
+        pred += predict_tree(X, tree)
 
-        pred = compute_pessimistic_predictions(y_signed, predictions=pred, tree_predictions=predict_tree(X, tree),
-                                               learning_rate=learning_rate, selected_probability=selected_probability)
+        prev_iteration_best_ids = numpy.take(candidate_trees_indices, numpy.argsort(candidate_losses)[1:n_kept_best])
+
         if verbose:
-            print(iteration, loss_function(pred), roc_auc_score(y, pred, sample_weight=w))
+            print(iteration, loss_function(pred))
 
     return utils.OBDTListClassifier(features, trees=new_trees)
-
